88a4953e88e3adf77a5d67735db138e0c9851837
Eyal Amir <eyalamirmusic@gmail.com>
2026-01-11 11:16:28 -0800

    Added cleaner helper functions to grab midi input/output ports as vectors

--- RTMIDI/Interface.cpp
+++ RTMIDI/Interface.cpp
@@ -3,15 +3,41 @@
 
 namespace RTMIDI
 {
+using StringVec = std::vector<std::string>;
 
+template<typename T>
+StringVec getPortNames()
+{
+    auto in = T();
+    auto res = StringVec();
+    auto size = in.getPortCount();
+    res.reserve(size);
+
+    for (size_t index = 0; index < size; ++index)
+        res.push_back(in.getPortName(index));
+
+    return res;
+}
+
+static StringVec getInputPortNames()
+{
+    return getPortNames<RtMidiIn>();
+}
+
+static StringVec getOutputPortNames()
+{
+    return getPortNames<RtMidiOut>();
+}
+
+static void debug(const StringVec& vec)
+{
+    for (auto& e: vec)
+        std::cout << e << std::endl;
+}
 
 void printPorts()
 {
-    auto in = RtMidiIn();
-
-    for (size_t index = 0; index < in.getPortCount(); ++index)
-    {
-        std::cout << in.getPortName(index) << std::endl;
-    }
+    debug(getInputPortNames());
+    debug(getOutputPortNames());
 }
-}
+} // namespace RTMIDI
